f9aaa59035b71445f521995864f0bfeb5e91154f
isobung <1024682836@qq.com>
2023-06-09 11:47:49 +0800

    add md5 code

---
+++ regex.cpp
@@ -0,0 +1,166 @@
+#include "openssl/md5.h"
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <iomanip>
+
+std::string MD5(const std::string& src)
+{
+    unsigned char     MD5Hash[MD5_DIGEST_LENGTH];
+    std::string       MD5Digest;
+    std::string       tmp;
+    std::stringstream ss;
+
+    MD5((const unsigned char*)src.c_str(), src.size(), MD5Hash);
+
+    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
+        // 2位16进制
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)MD5Hash[i] << std::endl;
+        ss >> tmp;
+        MD5Digest += tmp;
+    }
+
+    return MD5Digest;
+}
+
+void ParseWfd()
+{
+    std::string msg =
+        "RTSP/1.0 200 OK\r\nCSeq: 1\r\nPublic: org.wfa.wfd1.0, GET_PARAMETER, SET_PARAMETER, SETUP, "
+        "PLAY, PAUSE, TEARDOWN\r\n\r\nGET_PARAMETER rtsp://localhost/wfd1.0 RTSP/1.0\r\nCSeq: "
+        "1\r\nContent-Length: 130\r\n\r\nContent-Type: text/parameters\r\nwfd_audio_codecs\r\nwfd_video_formats\r\n";
+
+    std::regex               reg{"\r\n\r\n"};
+    std::vector<std::string> out{std::sregex_token_iterator(msg.begin(), msg.end(), reg, -1),
+                                 std::sregex_token_iterator()};
+    for (auto item : out) {
+        std::cout << item << std::endl << std::endl;
+    }
+}
+
+std::string Trim(const std::string& str)
+{
+    return str.substr(str.find_first_not_of(' '), str.find_last_not_of(' ') + 1 - str.find_first_not_of(' '));
+}
+
+void ParseRtsp()
+{
+    std::string              msg = "Response: RTSP/1.0 401 Unauthorized\r\n"
+                                   "CSeq: 3\r\n"
+                                   "WWW-Authenticate: Basic realm=\"IP Camera\"\r\n"
+                                   "WWW-Authenticate: Digest realm=\"IP Camera(C8121)\", "
+                                   "nonce=\"3559b2d4e6208048ffafbd8f6f69052f\", stale=\"FALSE\"\r\n"
+                                   "Date:  Sat, Feb 21 1970 13:35:11 GMT\r\n"
+                                   "\r\n";
+    std::regex               reg{"\r\n"};
+    std::vector<std::string> out{std::sregex_token_iterator(msg.begin(), msg.end(), reg, -1),
+                                 std::sregex_token_iterator()};
+    for (auto item : out) {
+        std::cout << item << std::endl << std::endl;
+    }
+
+    std::unordered_map<std::string, std::string> header;
+
+    for (int i = 0; i < (int)out.size(); ++i) {
+        if (out[i].size() > 3) {
+            auto index = out[i].find_first_of(':');
+            if (index != 0 && index + 1 != out[i].length()) {
+                auto token = Trim(out[i].substr(0, index));
+                auto value = Trim(out[i].substr(index + 1));
+                if (token == "WWW-Authenticate" && value.find("Digest") == std::string::npos) {
+                    continue;
+                }
+                header.emplace(token, value);
+                std::cout << "token:*" << token << ", value:*" << value << std::endl;
+            }
+        }
+    }
+    auto authenticate = header.at("WWW-Authenticate");
+    // std::cout << authenticate;
+    std::regex               regg{", "};
+    std::vector<std::string> auout{std::sregex_token_iterator(authenticate.begin(), authenticate.end(), regg, -1),
+                                   std::sregex_token_iterator()};
+
+    for (auto item : auout) {
+        auto separator = item.find('=');
+        if (separator != std::string::npos) {
+            auto key = item.substr(0, separator);
+            if (key == "Digest realm") {
+                auto value = item.substr(separator + 2);
+                value.pop_back();
+                std::cout << "Digest realm: " << value << std::endl;
+            } else if (key == "nonce") {
+                auto value = item.substr(separator + 2);
+                value.pop_back();
+                std::cout << "nonce: " << value << std::endl;
+            }
+        }
+    }
+}
+
+bool Parse(const std::string& url)
+{
+    if (url.empty()) {
+        return false;
+    }
+
+    std::regex  match("^rtsp://(([a-zA-Z0-9]+):([a-zA-Z0-9]+)@)?([a-zA-Z0-9.-]+)(:([0-9]+))?/");
+    std::smatch sm;
+    if (!std::regex_search(url, sm, match)) {
+        printf("Invalid URL\n");
+        return false;
+    }
+    // assert(sm.size() == 7);
+    auto        username_ = sm[2].str();
+    auto        password_ = sm[3].str();
+    auto        host_     = sm[4].str();
+    int         port      = std::atoi(sm[6].str().c_str());
+    std::string port_     = "";
+    if (port > 0) {
+        port_ = port;
+    }
+    auto path_ = sm.suffix().str();
+
+    printf("username: %s\npassword: %s\nhost: %s\nport: %d\npath: %s\n", username_.c_str(), password_.c_str(),
+           host_.c_str(), port_.c_str(), path_.c_str());
+    return true;
+}
+
+// Authorization: Digest username="admin", realm="IP Camera(C8121)", nonce="3559b2d4e6208048ffafbd8f6f69052f",
+// uri="rtsp://192.168.63.62:554?username=admin&password=nj123456", response="8f4cb30c7ef16e161bbe81a0c312ec29"\r\n
+void Response()
+{
+    std::string realm    = "IP Camera(C8121)";
+    std::string nonce    = "3559b2d4e6208048ffafbd8f6f69052f";
+    std::string username = "admin";
+    std::string password = "nj123456";
+    std::string rtspUrl  = "rtsp://192.168.63.62:554?username=admin&password=nj123456";
+
+    auto urpMD5   = MD5(username + ':' + realm + ':' + password);
+    std::cout << urpMD5 << " " << urpMD5.length() << std::endl;
+    if (urpMD5 == "8696d231bd07a83e244c0b61407adc76") {
+        std::cout << "urp pass\n";
+    }
+    auto pmuMD5   = MD5("DESCRIBE:" + rtspUrl);
+    if (pmuMD5 == "30c26a0ff3b414738bc4be026dfb7030") {
+        std::cout << "pmu pass\n";
+    }
+    std::cout << pmuMD5 << " " << pmuMD5.length() << std::endl;
+    auto responce = MD5(urpMD5 + ':' + nonce + ':' + pmuMD5);
+
+    std::cout << responce << std::endl;
+}
+
+int main()
+{
+    ParseRtsp();
+    // rtsp://192.168.63.188:554/mpeg4?username=admin&password=7AE44E20D1E6E851767189B0C57CAC64
+    // rtsp://192.168.63.62:554?username=admin&password=nj123456
+    // Response();
+    return 0;
+}
---
+++ taskpool.cpp
@@ -0,0 +1,201 @@
+#include <functional>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int32_t OnEvent(std::string& event)
+{
+    stringstream ss;
+    ss << this_thread::get_id();
+    std::string msg = "***thread id " + ss.str() + ", event come:" + event;
+    std::cout << msg << endl << flush;
+    int sum = 0;
+    // 注意这段代码可能会被某些默认是O2的编译脚本优化掉，要去掉优化
+    for (int i = 0; i < 1e9; ++i) {
+        sum += i;
+    }
+    return 0;
+}
+
+class TaskPool {
+public:
+    using Task      = int32_t(const std::string&);
+    using BoundTask = int32_t();
+    TaskPool();
+    ~TaskPool();
+    virtual void    PushTask(std::packaged_task<BoundTask>& task);
+    virtual void    Stop();
+    virtual int32_t Start(int32_t threadNum);
+
+protected:
+    virtual void TaskMainWorker();
+
+    bool                                      isRunning_ = false;
+    std::mutex                                taskMutex_;
+    std::vector<std::thread>                  threads_;
+    std::condition_variable                   hasTask_;
+    std::condition_variable                   acceptNewTask_;
+    std::chrono::microseconds                 timeoutInterval_;
+    std::deque<std::packaged_task<BoundTask>> tasks_;
+};
+TaskPool::TaskPool()
+{
+    cout << "task pool ctor\n" << flush;
+}
+TaskPool::~TaskPool()
+{
+    if (isRunning_) {
+        Stop();
+    }
+    cout << "task pool dtor\n" << flush;
+}
+
+int32_t TaskPool::Start(int32_t threadNum)
+{
+    if (!threads_.empty()) {
+        return -1;
+    }
+    isRunning_ = true;
+    threads_.reserve(threadNum);
+    for (int i = 0; i < threadNum; ++i) {
+        threads_.push_back(std::thread(&TaskPool::TaskMainWorker, this));
+        auto name = "thread" + std::to_string(i);
+        pthread_setname_np(threads_.back().native_handle(), name.c_str());
+    }
+    return 0;
+}
+
+void TaskPool::Stop()
+{
+    {
+        std::unique_lock<std::mutex> lock(taskMutex_);
+        isRunning_ = false;
+        hasTask_.notify_all();
+    }
+    for (auto& t : threads_) {
+        t.join();
+    }
+}
+
+void TaskPool::PushTask(std::packaged_task<BoundTask>& task)
+{
+    if (threads_.empty()) {
+    } else {
+        std::unique_lock<std::mutex> lock(taskMutex_);
+        while (tasks_.size() >= 10) {
+            cout << "task pool overload\n" << flush;
+            acceptNewTask_.wait(lock);
+        }
+        tasks_.emplace_back(std::move(task));
+        hasTask_.notify_one();
+    }
+}
+
+void TaskPool::TaskMainWorker()
+{
+    while (isRunning_) {
+        std::unique_lock<std::mutex> lock(taskMutex_);
+        if (tasks_.empty() && isRunning_) {
+            hasTask_.wait(lock);
+        } else {
+            std::packaged_task<BoundTask> task = std::move(tasks_.front());
+            tasks_.pop_front();
+            stringstream ss1;
+            ss1 << this_thread::get_id();
+            std::string msg1 = "***thread id " + ss1.str() + ", pop task\n";
+            std::cout << msg1 << flush;
+            acceptNewTask_.notify_one();
+            lock.unlock();
+            stringstream ss;
+            ss << this_thread::get_id();
+            std::string msg = "***thread id " + ss.str() + ", exec task\n";
+            std::cout << msg << flush;
+            task();
+        }
+    }
+}
+class EventManager : public TaskPool {
+public:
+    EventManager() { cout << "event manager ctor\n" << flush; }
+    ~EventManager() { cout << "event manager dtor\n" << flush; }
+    void StopEventLoop()
+    {
+        Stop();
+        eventThread_.join();
+    }
+    int32_t StartEventLoop()
+    {
+        int ret = Start(10);
+        if (ret != 0) {
+            return ret;
+        }
+        eventThread_ = std::thread(&EventManager::ProcessEvent, this);
+        pthread_setname_np(eventThread_.native_handle(), "eventloop");
+        return 0;
+    }
+    int32_t PushEvent(const std::string& event)
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        events_.emplace(event);
+        hasEvent_.notify_one();
+        return 0;
+    }
+
+private:
+    void ProcessEvent()
+    {
+        std::unique_lock<std::mutex> lock(mutex_);
+        while (isRunning_) {
+            if (events_.empty()) {
+                hasEvent_.wait(lock);
+            } else {
+                auto event = events_.front();
+                events_.pop();
+                std::packaged_task<BoundTask> task(std::bind(&OnEvent, event));
+                // std::cout << "==========push event: " << event << endl << flush;
+                PushTask(task);
+            }
+        }
+    }
+
+private:
+    std::mutex                   mutex_;
+    std::thread                  eventThread_;
+    std::condition_variable      hasEvent_;
+    std::queue<std::string>      events_;
+    std::unique_ptr<std::thread> eventLoop_ = nullptr;
+};
+
+void Test()
+{
+    EventManager* emgr = new EventManager();
+    emgr->StartEventLoop();
+
+    for (int i = 0; i < 100; ++i) {
+        emgr->PushEvent(std::to_string(i));
+    }
+}
+int main()
+{
+    // cout << "******enter for start\n" << flush;
+    // cin.get();
+    // std::thread t(&Test);
+    // pthread_setname_np(t.native_handle(), "test");
+    // if (t.joinable()) {
+    //     t.join();
+    // }
+    // cout << "******run done, entor for exit\n" << flush;
+    // cin.get();
+    // vector<uint8_t> vec{1, 2, 3};
+    // const char* p = reinterpret_cast<const char*>(vec.data());
+    // cout << *p;
+    cout << true;
+    return 0;
+}
